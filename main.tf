e0e2d9a633e3a96c31a75027c43402d2352e1ea2
Saif Almaliki <saif.wsm@gmail.com>
2023-04-16 16:38:11 +0200

    add Ingress load balancer

--- main.tf
+++ main.tf
@@ -25,11 +25,15 @@ resource "azurerm_resource_group" "aks-rg" {
 # The 'k8sCluster' is the locally given name for that resource that is only to be used as a reference inside the scope of the module.
 # The name and dns_prefix are used to define the cluster's name and DNS name
 resource "azurerm_kubernetes_cluster" "k8sCluster" {
-  name = "k8sCluster"
+  name = "${var.cluster_name}-${var.env_name}"
   location = azurerm_resource_group.aks-rg.location
   resource_group_name = azurerm_resource_group.aks-rg.name
   dns_prefix = "k8sCluster"
   tags = azurerm_resource_group.aks-rg.tags
+
+  # Enable the Ingress controller for the AKS cluster.
+  # This flag installs Ingress Nginx and also installs the ExternalDNS that can be used to manage DNS entries automatically.
+  http_application_routing_enabled = true
 
   # In the 'default_node_pool' you are defining the specs for the worker nodes.
   default_node_pool {
@@ -38,12 +42,22 @@ resource "azurerm_kubernetes_cluster" "k8sCluster" {
     vm_size = "standard_d2_v2"
   }
   # define the type of the identity, which is 'SystemAssigned'.
-  # This means that Azure will automatically create the required roles and permissions, 
+  # This means that Azure will automatically create the required roles and permissions,
   # and you won't need to manage any credentials.
   identity {
     type = "SystemAssigned"
   }
 }
+
+
+# Modify the cluster by Adding another - more memory-optimized node pool to your cluster for your memory-hungry applications.
+resource "azurerm_kubernetes_cluster_node_pool" "memoryOptimizedPoll" {
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8sCluster.id
+  name = "memoryopt"
+  node_count = 1
+  vm_size = "standard_d11_v2"
+}
+
 
 
 
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+    description = "The Name for the AKS cluster"
+    default = "k8sCluster"
+}
+
+variable "env_name" {
+    description = "The Environment for the AKS Cluster"
+    default = "dev"
+}
